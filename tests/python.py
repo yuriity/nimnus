86944032c245433a7b2196cd38eed1f9ce784039
Yurii Tykhovskyi <yuriity@outlook.com>
2026-01-19 10:22:04 +0200

    Mon Jan 19 10:21:57 EET 2026

--- tests/python.py
+++ tests/python.py
@@ -1,80 +1,41 @@
-from flask import Flask, jsonify, request, send_from_directory
-from utility.hash_util import hash_string_256, hash_block
+import os
+import sublime
+from pathlib import PurePath
 
-from wallet import Wallet
+FONT = "Iosevka Nerd Font"
 
+#   …/nimnus   main ⇣1    v20.19.6
 
-class Dog:
-
-    def __init__(self, name):
-        self.name = name
-
-    def get_name(self):
-        return self.name
-
-
-class Verification:
-
-    @staticmethod
-    def valid_proof(transactions, last_hash, proof):
-        """Validate a proof of work number and see if it solves the puzzle algorithm (two leading 0s)
-
-        Arguments:
-            :transactions: The transactions of the block for which the proof is created.
-            :last_hash: The previous block's hash which will be stored in the current block.
-            :proof: The proof number we're testing.
-        """
-        ordered_transactions = [
-            tx.to_ordered_dict()
-            for tx in transactions
-        ]
-        guess = (str(ordered_transactions) +
-                 str(last_hash) + str(proof)).encode()
-        guess_hash = hash_string_256(guess)
-        # print(f'guess_hash: {guess_hash}')
-
-        return guess_hash[0:2] == '00'
-
-    @classmethod
-    def verify_chain(cls, blockchain):
-        """ Verify the current blockchein and return True if it's valid. """
-        # print('  verify_chain()')
-        for (index, block) in enumerate(blockchain):
-            if index == 0:
-                continue
-            if block.previous_hash != hash_block(blockchain[index - 1]):
-                return False
-            if not cls.valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
-                print(f'Proof of work is invalid! - {block.proof}')
-                return False
-        return True
-
-    @staticmethod
-    def verify_transaction(transaction, get_balance, check_funds=True):
-        """ Checks whether the sender has enough coins. """
-        if check_funds:
-            sender_balance = get_balance(transaction.sender)
-            return sender_balance >= transaction.amount and Wallet.verify_transaction(transaction)
-        else:
-            return Wallet.verify_transaction(transaction)
-
-    @classmethod
-    def verify_transactions(cls, open_transactions, get_balance):
-        """ Verifies all open transactions. """
-        return all([
-            cls.verify_transaction(tx, get_balance, False)
-            for tx in open_transactions
-        ])
+# ABCDEFGHIJKLMNOPQRSTUVWXYZ
+# abcdefghijklmnopqrstuvwxyz
+# 1234567890
+ABCDEFGHIJKLMNOPQRSTUVWXYZ = ""
+abcdefghijklmnopqrstuvwxyz = ""
+NUMERALS = 1234567890
+SIMILAR = "oO08 iIlL1 g9qCGQ 8%& <([{}])> .,;: ~-_="
+DIACRITICS_ETC = "â é ù ï ø ç Ã Ē Æ œ"
 
 
-app = Flask(__name__)
+class SideBarDuplicateCommand(SideBarCommand):
 
+    def run(self, paths, **kwargs):
+        source = self.get_path(paths, **kwargs)
+        base, leaf = os.path.split(source)
 
-@app.route('/', methods=['GET'])
-def get_node_ui():
-    return send_from_directory('ui', 'node.html')
+        # find the file extension
+        name, ext = os.path.splitext(leaf)
+        if ext != '':
+            while '.' in name:
+                name, _ext = os.path.splitext(name)
+                ext = _ext + ext
+                if _ext == '':
+                    break
 
+        source = self.get_path(paths, **kwargs)
 
-@app.route('/network', methods=['GET'])
-def get_network_ui():
-    return send_from_directory('ui', 'network.html')
+        input_panel = self.window.show_input_panel(
+            'Duplicate As:', source, partial(self.on_done, source), None, None)
+
+        input_panel.sel().clear()
+        input_panel.sel().add(
+            sublime.Region(len(base) + 1, len(source) - len(ext)))
